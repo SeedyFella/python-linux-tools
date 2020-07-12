543d88c53b69c2fd74ec0663f4613ed3815f34df
Zach Wingrave <z.wingrave@gmail.com>
2020-07-12 15:01:33 +1000

    Updated to PEP-8 standard.

--- scripts/purge_bash_history.py
+++ scripts/purge_bash_history.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+"""Creates a copy of the ~/.bash_history file, excluding
+lines which contain keywords defined in a list."""
 from os import chmod, remove
 from os.path import expanduser, isfile
 
@@ -9,35 +11,34 @@ new_history_path = home + "/.bash_history_new"
 
 # Open the ~/.bash_history file to be purged.
 try:
-  old_history = open(old_history_path, "r")
+    old_history = open(old_history_path, "r")
 except FileNotFoundError:
-  print("ERROR:", old_history_path, "not found.")
-  raise SystemExit(0)
+    print("ERROR:", old_history_path, "not found.")
+    raise SystemExit(0)
 
 # Check if ~/.bash_history_new already exists.
 try:
-  if isfile(new_history_path):
-    remove(new_history_path)
-  new_history = open(new_history_path, "x")
+    if isfile(new_history_path):
+        remove(new_history_path)
+    new_history = open(new_history_path, "x")
 except FileExistsError:
-  print("ERROR:", new_history_path, "already exists.")
-  old_history.close()
-  raise SystemExit(0)
+    print("ERROR:", new_history_path, "already exists.")
+    old_history.close()
+    raise SystemExit(0)
 
 # The strings to be purged. Edit this list to suit your needs.
 purge_strings = [
-  "clear",
-  "exit",
-  "ls",
-  "ping",
-  "time"
+    "clear",
+    "exit",
+    "ls",
+    "ping",
+    "time"
 ]
 
 # Copy the old history file into the new one, excluding purge strings.
 for line in old_history:
-  if any(word in line for word in purge_strings):
-    continue
-  else:
+    if any(word in line for word in purge_strings):
+        continue
     new_history.write(line)
 
 # Close all files.
